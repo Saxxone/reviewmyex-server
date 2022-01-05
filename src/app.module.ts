bdf66723af57a436d33413afd14036b3809c1478
saxxone <saxxone17@gmail.com>
2022-01-05 21:36:42 +0100

    changed db user and password

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,8 +11,8 @@ import { ReviewsModule } from './reviews/reviews.module';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'db_master',
-      password: 'hJO$EC2xwA9it5T', //hJO$EC2xwA9it5T
+      username: 'admin',
+      password: '*hJO$EC2xwA9it5T', //*hJO$EC2xwA9it5T
       database: 'review_my_ex',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
